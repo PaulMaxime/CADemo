9fd0623ed02460ea8784d742e06fefcfb54effa4
Paul M Franceus <paul@blueiris.us>
2011-07-20 20:29:29 -0600

    Initial Commit

---
+++ CADemo/CADemoAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  CADemoAppDelegate.h
+//  CADemo
+//
+//  Created by Paul Franceus on 7/20/11.
+//  Copyright 2011 Google, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CADemoAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
